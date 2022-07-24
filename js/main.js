b87dd8be28d2de4fcab63a1e00e2128160adebce
Mubinabegim <mubinabegimxayrullayeva@gmail.com>
2022-07-24 09:08:58 +0500

    doc: complete the project

---
+++ js/main.js
@@ -0,0 +1,194 @@
+const rootElement = document.getElementById("root");
+const root = ReactDOM.createRoot(rootElement);
+    wrapper = React.createElement('div', {className: 'wrapper'},[
+        React.createElement('div', {className: 'icon-heart'},[
+            React.createElement('i', {className: 'bi bi-heart'})            
+        ]),
+        React.createElement('div', {className: 'pokemon-logo'},[
+            React.createElement('img', {src: './images/pokemon_logo.svg', alt:'pokemon-logo'})
+        ]),
+        React.createElement('div', {className: "form-elements"},[
+            React.createElement('form', {id: 'form'},[
+                React.createElement('div', {className: 'form-element'},[
+                    React.createElement('select', {id: 'gross', className: 'gross'},[
+                        React.createElement('option', {value: 'gross'}, 'Gross')
+                    ])
+                    
+                ]),
+                React.createElement('div', {className: 'form-element'},[
+                    React.createElement('input', {className: 'search', type: 'search', placeholder:'Search       🔍'})
+                ]),
+                React.createElement('div', {className: 'form-element'},[
+                    React.createElement('select', {className: 'letters', id: 'letters'},[
+                        React.createElement('option', {value: 'Aa - Zz'}, 'Aa - Zz')
+                    ])
+                ]),
+                React.createElement('div', {className: 'form-element'},[
+                    React.createElement('button', {className: 'form-btn', typeof: 'submit'}, 'Button')
+                ])          
+            ])
+        ]),
+        React.createElement('div', {className: 'row'}, [
+            React.createElement('div', {className: 'col-3'}, [
+                React.createElement('div', {className: 'card'},[
+                    React.createElement('div', {className: 'card-header'},[
+                        React.createElement('img', {src: './images/dragon.jpg', alt:'dragon'})
+                    ]),
+                    React.createElement('div', {className: 'card-body'},[
+                        React.createElement('div', {className: 'card-text'},[
+                            React.createElement('h2', null, 'Pokemons'),
+                            React.createElement('p', null, "Grass, Poison")
+                        ]),
+                        React.createElement('div', {className: 'card-icon'},[
+                            React.createElement('i', {className: 'bi bi-heart'})
+                        ])
+                    ]),
+                    React.createElement('div', {className: 'card-footer'},[
+                        React.createElement('h2', null, '6.9 kg'),
+                        React.createElement('h2', null, '99 age')
+                    ])
+                ])
+            ]),
+            React.createElement('div', {className: 'col-3'}, [
+                React.createElement('div', {className: 'card'},[
+                    React.createElement('div', {className: 'card-header'},[
+                        React.createElement('img', {src: './images/dragon.jpg', alt:'dragon'})
+                    ]),
+                    React.createElement('div', {className: 'card-body'},[
+                        React.createElement('div', {className: 'card-text'},[
+                            React.createElement('h2', null, 'Pokemons'),
+                            React.createElement('p', null, "Grass, Poison")
+                        ]),
+                        React.createElement('div', {className: 'card-icon'},[
+                            React.createElement('i', {className: 'bi bi-heart'})
+                        ])
+                    ]),
+                    React.createElement('div', {className: 'card-footer'},[
+                        React.createElement('h2', null, '6.9 kg'),
+                        React.createElement('h2', null, '99 age')
+                    ])
+                ])
+            ]),
+            React.createElement('div', {className: 'col-3'}, [
+                React.createElement('div', {className: 'card'},[
+                    React.createElement('div', {className: 'card-header'},[
+                        React.createElement('img', {src: './images/dragon.jpg', alt:'dragon'})
+                    ]),
+                    React.createElement('div', {className: 'card-body'},[
+                        React.createElement('div', {className: 'card-text'},[
+                            React.createElement('h2', null, 'Pokemons'),
+                            React.createElement('p', null, "Grass, Poison")
+                        ]),
+                        React.createElement('div', {className: 'card-icon'},[
+                            React.createElement('i', {className: 'bi bi-heart'})
+                        ])
+                    ]),
+                    React.createElement('div', {className: 'card-footer'},[
+                        React.createElement('h2', null, '6.9 kg'),
+                        React.createElement('h2', null, '99 age')
+                    ])
+                ])
+            ]),
+            React.createElement('div', {className: 'col-3'}, [
+                React.createElement('div', {className: 'card'},[
+                    React.createElement('div', {className: 'card-header'},[
+                        React.createElement('img', {src: './images/dragon.jpg', alt:'dragon'})
+                    ]),
+                    React.createElement('div', {className: 'card-body'},[
+                        React.createElement('div', {className: 'card-text'},[
+                            React.createElement('h2', null, 'Pokemons'),
+                            React.createElement('p', null, "Grass, Poison")
+                        ]),
+                        React.createElement('div', {className: 'card-icon'},[
+                            React.createElement('i', {className: 'bi bi-heart'})
+                        ])
+                    ]),
+                    React.createElement('div', {className: 'card-footer'},[
+                        React.createElement('h2', null, '6.9 kg'),
+                        React.createElement('h2', null, '99 age')
+                    ])
+                ])
+            ]),
+            React.createElement('div', {className: 'col-3'}, [
+                React.createElement('div', {className: 'card'},[
+                    React.createElement('div', {className: 'card-header'},[
+                        React.createElement('img', {src: './images/dragon.jpg', alt:'dragon'})
+                    ]),
+                    React.createElement('div', {className: 'card-body'},[
+                        React.createElement('div', {className: 'card-text'},[
+                            React.createElement('h2', null, 'Pokemons'),
+                            React.createElement('p', null, "Grass, Poison")
+                        ]),
+                        React.createElement('div', {className: 'card-icon'},[
+                            React.createElement('i', {className: 'bi bi-heart'})
+                        ])
+                    ]),
+                    React.createElement('div', {className: 'card-footer'},[
+                        React.createElement('h2', null, '6.9 kg'),
+                        React.createElement('h2', null, '99 age')
+                    ])
+                ])
+            ]),
+            React.createElement('div', {className: 'col-3'}, [
+                React.createElement('div', {className: 'card'},[
+                    React.createElement('div', {className: 'card-header'},[
+                        React.createElement('img', {src: './images/dragon.jpg', alt:'dragon'})
+                    ]),
+                    React.createElement('div', {className: 'card-body'},[
+                        React.createElement('div', {className: 'card-text'},[
+                            React.createElement('h2', null, 'Pokemons'),
+                            React.createElement('p', null, "Grass, Poison")
+                        ]),
+                        React.createElement('div', {className: 'card-icon'},[
+                            React.createElement('i', {className: 'bi bi-heart'})
+                        ])
+                    ]),
+                    React.createElement('div', {className: 'card-footer'},[
+                        React.createElement('h2', null, '6.9 kg'),
+                        React.createElement('h2', null, '99 age')
+                    ])
+                ])
+            ]),
+            React.createElement('div', {className: 'col-3'}, [
+                React.createElement('div', {className: 'card'},[
+                    React.createElement('div', {className: 'card-header'},[
+                        React.createElement('img', {src: './images/dragon.jpg', alt:'dragon'})
+                    ]),
+                    React.createElement('div', {className: 'card-body'},[
+                        React.createElement('div', {className: 'card-text'},[
+                            React.createElement('h2', null, 'Pokemons'),
+                            React.createElement('p', null, "Grass, Poison")
+                        ]),
+                        React.createElement('div', {className: 'card-icon'},[
+                            React.createElement('i', {className: 'bi bi-heart'})
+                        ])
+                    ]),
+                    React.createElement('div', {className: 'card-footer'},[
+                        React.createElement('h2', null, '6.9 kg'),
+                        React.createElement('h2', null, '99 age')
+                    ])
+                ])
+            ]),
+            React.createElement('div', {className: 'col-3'}, [
+                React.createElement('div', {className: 'card'},[
+                    React.createElement('div', {className: 'card-header'},[
+                        React.createElement('img', {src: './images/dragon.jpg', alt:'dragon'})
+                    ]),
+                    React.createElement('div', {className: 'card-body'},[
+                        React.createElement('div', {className: 'card-text'},[
+                            React.createElement('h2', null, 'Pokemons'),
+                            React.createElement('p', null, "Grass, Poison")
+                        ]),
+                        React.createElement('div', {className: 'card-icon'},[
+                            React.createElement('i', {className: 'bi bi-heart'})
+                        ])
+                    ]),
+                    React.createElement('div', {className: 'card-footer'},[
+                        React.createElement('h2', null, '6.9 kg'),
+                        React.createElement('h2', null, '99 age')
+                    ])
+                ])
+            ])
+        ])
+    ]);
+root.render(wrapper)
